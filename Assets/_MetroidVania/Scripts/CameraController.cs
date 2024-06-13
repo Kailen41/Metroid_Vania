dec00fc9fd2b624c3c33ced837fce31050a6fbb5
Kailen41 <csilveira81806@gmail.com>
2024-06-13 13:30:04 -0400

    Updated the camera follow script to incorporate bounds
    -Added a no collision layer into the project and placed that layer on the newly added box collider that is being used as a camera bounds component.
    -In Awake(), getting the half height and half width of the camera aspect and orthographic size.
    -Clamping the camera based on the collider's X and Y, plus adding the width to min x and subtracting the width to max x. Doing the same on the y.

--- Assets/_MetroidVania/Scripts/CameraController.cs
+++ Assets/_MetroidVania/Scripts/CameraController.cs
@@ -6,16 +6,26 @@ public class CameraController : MonoBehaviour
 {
     private PlayerController _player;
 
+    public BoxCollider2D boundsBox;
+
+    private float _halfHeight, _halfWidth;
+
     private void Awake()
     {
         _player = FindAnyObjectByType<PlayerController>();
+
+        _halfHeight = Camera.main.orthographicSize;
+        _halfWidth = _halfHeight * Camera.main.aspect;
     }
 
     void Update()
     {
         if (_player != null)
         {
-            transform.position = new Vector3(_player.transform.position.x, _player.transform.position.y, transform.position.z);
+            transform.position = new Vector3(
+                Mathf.Clamp(_player.transform.position.x, boundsBox.bounds.min.x + _halfWidth, boundsBox.bounds.max.x - _halfWidth),
+                Mathf.Clamp(_player.transform.position.y, boundsBox.bounds.min.y + _halfHeight, boundsBox.bounds.max.y - _halfHeight), 
+                transform.position.z);
         }
     }
 }
