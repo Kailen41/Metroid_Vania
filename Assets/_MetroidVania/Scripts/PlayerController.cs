dfdcacb11f7b5e63b1e5bad5a741c5b6b89076fb
Kailen41 <93565307+Kailen41@users.noreply.github.com>
2024-06-12 20:18:36 -0400

    Jumping, only jump once, no sticky colliders
    -Player can now jump using the space bar, which changes the Y velocity by the amount of jumpForce.
    -Made a check to see if the player is on the ground using the layer system in Unity. If the layer is the Ground layer, the player will be able to jump. If not, the player can't.
    -Using the bool _isGrounded and an overlap circle to check the groundpoint position, which is childed to the player and moved to the feet area, a radius size for the circle, and LayerMask to see what is ground.
    -Player would get stuck on the colliders when jumping or moving down a level. Created a physical material 2D called SlipperyMat_2D, which removes all friction from the collider.

--- Assets/_MetroidVania/Scripts/PlayerController.cs
+++ Assets/_MetroidVania/Scripts/PlayerController.cs
@@ -6,9 +6,15 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D _theRB;
 
-    [Header("Player Movment")]
+    [Header("Player Movement")]
     public float moveSpeed;
     public float jumpForce;
+
+    public Transform groundPoint;
+    public LayerMask groundLayer;
+
+    private bool _isGrounded;
+    private float _overlapRadius = 0.2f;
 
     private void Awake()
     {
@@ -24,5 +30,11 @@ public class PlayerController : MonoBehaviour
     {
         float _xInput = Input.GetAxisRaw("Horizontal");
         _theRB.velocity = new Vector2(_xInput * moveSpeed, _theRB.velocity.y);
+
+        _isGrounded = Physics2D.OverlapCircle(groundPoint.position, _overlapRadius, groundLayer);
+        if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
+        {
+            _theRB.velocity = new Vector2(_theRB.velocity.x, jumpForce);
+        }
     }
 }
